512e2f2510f3c14e7e9f36c2f9db83d036c3cfdc
removed-user <caseyriley94@gmail.com>
2026-06-28 00:59:10 -0400

    Update commit hash for guix channel

--- Channels/PerChannel/scheme/guix.scm
+++ Channels/PerChannel/scheme/guix.scm
@@ -4,6 +4,6 @@
   (branch "master")
   (introduction
     (make-channel-introduction
-      "9edb3f66fd807b096b48283debdcddccfea34bad"
+      "9f6d76535cd08a6cfa6c3133b639d745413091a4"
       (openpgp-fingerprint
         "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
