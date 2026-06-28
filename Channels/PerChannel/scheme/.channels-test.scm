22fe44c13bf33d6187d59326f78e29a95473112f
casey <caseyriley94@gmail.com>
2026-06-28 00:52:44 -0400

    channels-test.scm for evaluation test

---
+++ Channels/PerChannel/scheme/.channels-test.scm
@@ -0,0 +1,7 @@
+(list 
+load "/home/casey/guix/guix-init/Channels/PerChannel/scheme/guix.scm"
+load "/home/casey/guix/guix-init/Channels/PerChannel/scheme/nonguix.scm"
+load "/home/casey/guix/guix-init/Channels/PerChannel/scheme/guix-science.scm"
+load "/home/casey/guix/guix-init/Channels/PerChannel/scheme/guix-past.scm"
+ )
+
